3a0c985c7b7e24212e4b8e867f282735cd78ca80
kikeai <lg5967133@gmail.com>
2023-04-19 19:32:50 -0400

    style cloud button

--- src/Components/button/cloudButton.tsx
+++ src/Components/button/cloudButton.tsx
@@ -13,9 +13,48 @@ const CloudButton = ({ seter }: Props) => {
       {
         cloudName,
         uploadPreset,
-        cropping: true, // add a cropping step
+        sources: [
+          'local',
+          'url',
+          'camera',
+          'google_drive',
+          'dropbox'
+        ],
+        showAdvancedOptions: false,
+        cropping: true,
+        multiple: false,
+        defaultSource: 'local',
         clientAllowedFormats: ['jpg', 'jpeg', 'png'],
-        maxImageWidth: 600
+        maxImageWidth: 600,
+        styles: {
+          palette: {
+            window: '#ffffff',
+            sourceBg: '#f4f4f5',
+            windowBorder: '#90a0b3',
+            tabIcon: '#000000',
+            inactiveTabIcon: '#555a5f',
+            menuIcons: '#555a5f',
+            link: '#0433ff',
+            action: '#339933',
+            inProgress: '#0433ff',
+            complete: '#339933',
+            error: '#cc0000',
+            textDark: '#000000',
+            textLight: '#fcfffd'
+          },
+          fonts: {
+            default: null,
+            'sans-serif': {
+              url: null,
+              active: true
+            }
+          }
+        }
+        // cloudName,
+        // uploadPreset,
+        // cropping: true, // add a cropping step
+        // clientAllowedFormats: ['jpg', 'jpeg', 'png'],
+        // maxImageWidth: 600
       },
       (error, result) => {
         if (!error && result && result.event === 'success') {
@@ -28,8 +67,8 @@ const CloudButton = ({ seter }: Props) => {
   return (
     <button
     onClick={handleWidget}
-    id="upload_widget"
-    className=" bg-gray-900 px-2 py-2 rounded-full border border-white absolute bottom-0 right-0">
+    id='upload_widget'
+    className=' bg-gray-900 px-2 py-2 rounded-full border border-white absolute bottom-0 right-0'>
       <CameraIcon />
     </button>
   )
